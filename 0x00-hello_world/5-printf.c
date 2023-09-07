8113613701a9c150d644b52643abfd1e1b20ed59
Bee-brith <olajidebidemi0@gmail.com>
2023-09-07 05:33:33 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the main func
+ * Return: 0 when correct
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
